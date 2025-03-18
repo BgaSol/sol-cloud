4f3419f9b1ea808e0ffd6a59c279fc611a7fea6a
sol <bgasolyang@gmail.com>
2025-03-18 17:54:35 +0800

    refactor(mybatisPlus): Rename the configuration class and adjust the pagination plugin settings

--- cloud/plugin/plugin-database/src/main/java/com/bgasol/plugin/mybatisPlus/config/MybatisPlusInterceptorConfig.java
+++ cloud/plugin/plugin-database/src/main/java/com/bgasol/plugin/mybatisPlus/config/MybatisPlusInterceptorConfig.java
@@ -1,18 +1,17 @@
 package com.bgasol.plugin.mybatisPlus.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PostgresMybatisPlusInterceptorConfig {
-    @Bean("PostgresMybatisPlusInterceptor")
+public class MybatisPlusInterceptorConfig {
+    @Bean()
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         // 分页插件
-        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.POSTGRE_SQL);
-        paginationInnerInterceptor.setMaxLimit(1000L);
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        paginationInnerInterceptor.setMaxLimit(10000L);
 
         // 初始化MybatisPlus拦截器
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
