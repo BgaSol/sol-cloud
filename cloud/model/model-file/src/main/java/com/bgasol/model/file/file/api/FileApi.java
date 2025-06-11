080255cd9ffde36166b266fa5a35a089358afa89
Sol <bgasolyang@gmail.com>
2025-06-11 15:49:16 +0800

    feat(file): Add file update interface and optimize files API

--- cloud/model/model-file/src/main/java/com/bgasol/model/file/file/api/FileApi.java
+++ cloud/model/model-file/src/main/java/com/bgasol/model/file/file/api/FileApi.java
@@ -3,15 +3,13 @@ package com.bgasol.model.file.file.api;
 import com.bgasol.common.constant.value.FileConfigValues;
 import com.bgasol.common.core.base.vo.BaseVo;
 import com.bgasol.model.file.file.dto.FileCreateDto;
+import com.bgasol.model.file.file.dto.FileUpdateDto;
 import com.bgasol.model.file.file.entity.FileEntity;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(
         path = "/" + FileConfigValues.SERVICE_NAME + "/file",
@@ -21,6 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 public interface FileApi {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     BaseVo<FileEntity> save(FileCreateDto fileCreateDto);
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    BaseVo<FileEntity> update(FileUpdateDto fileUpdateDto);
 
     @DeleteMapping("/{ids}")
     BaseVo<Integer[]> delete(@PathVariable("ids") String ids);
