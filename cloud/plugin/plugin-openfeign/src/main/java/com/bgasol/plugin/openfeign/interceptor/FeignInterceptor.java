b5863c78dfe63beb5c3e782323273e9b1e9d64ea
sol <bgasolyang@gmail.com>
2025-09-16 18:09:59 +0800

    fix(plugin-openfeign): Fix the logic for adding user identity tokens in Feign requests

--- cloud/plugin/plugin-openfeign/src/main/java/com/bgasol/plugin/openfeign/interceptor/FeignInterceptor.java
+++ cloud/plugin/plugin-openfeign/src/main/java/com/bgasol/plugin/openfeign/interceptor/FeignInterceptor.java
@@ -1,23 +1,32 @@
 package com.bgasol.plugin.openfeign.interceptor;
 
+import cn.dev33.satoken.exception.NotWebContextException;
 import cn.dev33.satoken.same.SaSameUtil;
 import cn.dev33.satoken.stp.StpUtil;
 import com.bgasol.common.constant.value.GatewayConfigValues;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class FeignInterceptor implements RequestInterceptor {
 
     // feign拦截器, 在feign请求发出之前，加入一些操作
     @Override
     public void apply(RequestTemplate requestTemplate) {
+        // 添加来自网关的标识
+        requestTemplate.header(GatewayConfigValues.XFromGateway, "false");
         // 添加Same-Token请求头
         requestTemplate.header(SaSameUtil.SAME_TOKEN, SaSameUtil.getToken());
         // 添加用户身份令牌
-        requestTemplate.header(StpUtil.getTokenName(), StpUtil.getTokenValue());
-        // 添加来自网关的标识
-        requestTemplate.header(GatewayConfigValues.XFromGateway, "false");
+        try {
+            if (StpUtil.isLogin()) {
+                requestTemplate.header(StpUtil.getTokenName(), StpUtil.getTokenValue());
+            }
+        } catch (NotWebContextException e) {
+            log.debug(e.getMessage());
+        }
     }
 }
