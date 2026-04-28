fa778aecc0ae84b25a7ab478737b8014ac96f57c
sol <bgasolyang@gmail.com>
2026-04-28 23:04:35 +0800

    feat: add automatic interface scanning and standardize latest commit message format

--- cloud/common/common-auth/src/main/java/com/bgasol/plugin/satoken/runner/ControllerScanner.java
+++ cloud/common/common-auth/src/main/java/com/bgasol/plugin/satoken/runner/ControllerScanner.java
@@ -1,10 +1,15 @@
 package com.bgasol.plugin.satoken.runner;
 
 import cn.dev33.satoken.annotation.SaCheckPermission;
+import com.bgasol.model.system.menu.entity.MenuEntity;
 import com.bgasol.model.system.permission.api.PermissionApi;
 import com.bgasol.model.system.permission.dto.PermissionCreateDto;
 import com.bgasol.model.system.permission.dto.PermissionUpdateDto;
 import com.bgasol.model.system.permission.entity.PermissionEntity;
+import com.bgasol.model.system.role.api.RoleApi;
+import com.bgasol.model.system.role.dto.RoleCreateDto;
+import com.bgasol.model.system.role.dto.RoleUpdateDto;
+import com.bgasol.model.system.role.entity.RoleEntity;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +18,6 @@ import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.event.InstanceRegisteredEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.method.HandlerMethod;
@@ -21,8 +25,13 @@ import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -33,18 +42,19 @@ public class ControllerScanner {
     private String contextPath;
 
     private final PermissionApi permissionApi;
+    private final RoleApi roleApi;
 
     private final RequestMappingHandlerMapping requestMappingHandlerMapping;
 
     @EventListener(InstanceRegisteredEvent.class)
     public void scanControllers() throws InterruptedException {
-
         Thread.sleep(TimeUnit.SECONDS.toMillis(5));
 
         log.info("Scanning controllers...");
 
-        Map<RequestMappingInfo, HandlerMethod> handlerMethods =
-                requestMappingHandlerMapping.getHandlerMethods();
+        Map<String, RoleEntity> roles = new HashMap<>();
+        Map<String, PermissionEntity> permissions = new HashMap<>();
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = requestMappingHandlerMapping.getHandlerMethods();
 
         for (Map.Entry<RequestMappingInfo, HandlerMethod> entry : handlerMethods.entrySet()) {
 
@@ -54,14 +64,96 @@ public class ControllerScanner {
             Class<?> controllerClass = handlerMethod.getBeanType();
             Method method = handlerMethod.getMethod();
 
-            scanMethod(controllerClass, method, mappingInfo);
+            scanMethod(controllerClass, method, mappingInfo, permissions, roles);
         }
+        Map<String, PermissionEntity> oldPermissions = permissionApi.findByIds(permissions.keySet(), false)
+                .getData()
+                .stream()
+                .collect(Collectors.toMap(PermissionEntity::getId, Function.identity()));
+
+        // 老数据中有的则更新，没有的新增
+        permissions.forEach((key, value) -> {
+            if (oldPermissions.containsKey(key)) {
+                PermissionEntity permission = permissionApi.apply(
+                        PermissionUpdateDto.builder()
+                                .id(value.getId())
+                                .type(value.getType())
+                                .description(value.getDescription())
+                                .parentId(value.getParentId())
+                                .name(value.getName())
+                                .code(value.getCode())
+                                .path(value.getPath())
+                                .microService(value.getMicroService())
+                                .build()
+                ).getData();
+                log.info("apply permission:{}", permission.getName());
+
+            } else {
+                PermissionEntity permission = permissionApi.insert(
+                        PermissionCreateDto.builder()
+                                .id(value.getId())
+                                .type(value.getType())
+                                .description(value.getDescription())
+                                .parentId(value.getParentId())
+                                .name(value.getName())
+                                .code(value.getCode())
+                                .path(value.getPath())
+                                .microService(value.getMicroService())
+                                .build()
+                ).getData();
+                log.info("insert permission:{}", permission.getName());
+
+            }
+        });
+        Map<String, RoleEntity> oldRoles = roleApi.findByIds(roles.keySet(), true)
+                .getData()
+                .stream()
+                .collect(Collectors.toMap(RoleEntity::getId, Function.identity()));
+
+        roles.forEach((key, value) -> {
+            if (oldRoles.containsKey(key)) {
+                RoleEntity oldRole = oldRoles.get(key);
+
+                oldRole.getPermissions().removeIf(permission -> contextPath.equals(permission.getMicroService()));
+                oldRole.getPermissions().addAll(value.getPermissions());
+
+                RoleEntity role = roleApi.apply(RoleUpdateDto.builder()
+                        .id(value.getId())
+                        .code(value.getId())
+                        .name(oldRole.getName())
+                        .menuIds(oldRole.getMenus()
+                                .stream()
+                                .map(MenuEntity::getId)
+                                .collect(Collectors.toList()))
+                        .permissionIds(oldRole.getPermissions()
+                                .stream()
+                                .map(PermissionEntity::getId)
+                                .collect(Collectors.toList()))
+                        .build()).getData();
+                log.info("apply role:{}", role.getCode());
+
+            } else {
+                RoleEntity role = roleApi.insert(RoleCreateDto.builder()
+                        .code(value.getId())
+                        .name(value.getId())
+                        .permissionIds(value.getPermissions()
+                                .stream()
+                                .map(PermissionEntity::getId)
+                                .collect(Collectors.toList()))
+                        .menuIds(List.of())
+                        .build()).getData();
+                log.info("insert role:{}", role.getCode());
+
+            }
+        });
     }
 
     private void scanMethod(
             Class<?> controllerClass,
             Method method,
-            RequestMappingInfo mappingInfo) {
+            RequestMappingInfo mappingInfo,
+            Map<String, PermissionEntity> permissions,
+            Map<String, RoleEntity> roles) {
 
         if (!method.isAnnotationPresent(SaCheckPermission.class)) {
             return;
@@ -87,39 +179,15 @@ public class ControllerScanner {
             controllerDescription =
                     controllerClass.getAnnotation(Tag.class).name();
         }
-
-        PermissionEntity parentPermission =
-                permissionApi.findById(controllerName, false).getData();
-
-        if (ObjectUtils.isEmpty(parentPermission)) {
-
-            parentPermission = permissionApi.insert(
-                    PermissionCreateDto.builder()
-                            .parentId(null)
-                            .microService(contextPath)
-                            .path(controllerPath)
-                            .id(controllerName)
-                            .name(controllerName)
-                            .description(controllerDescription)
-                            .build()
-            ).getData();
-
-            log.info("insert parent permission:{}", parentPermission.getName());
-
-        } else {
-
-            parentPermission = permissionApi.apply(
-                    PermissionUpdateDto.builder()
-                            .microService(contextPath)
-                            .path(controllerPath)
-                            .id(controllerName)
-                            .name(controllerName)
-                            .description(controllerDescription)
-                            .build()
-            ).getData();
-
-            log.info("apply parent permission:{}", parentPermission.getName());
-        }
+        PermissionEntity parentPermission = PermissionEntity.builder()
+                .parentId(null)
+                .microService(contextPath)
+                .path(controllerPath)
+                .id(controllerName)
+                .name(controllerName)
+                .description(controllerDescription)
+                .build();
+        permissions.put(parentPermission.getId(), parentPermission);
 
         // Method描述
         String description = "";
@@ -142,48 +210,37 @@ public class ControllerScanner {
                 .findFirst()
                 .orElse("");
 
+        SaCheckPermission saCheckPermission = method.getAnnotation(SaCheckPermission.class);
         // 权限
-        String permissionValue =
-                method.getAnnotation(SaCheckPermission.class).value()[0];
+        String permissionValue = saCheckPermission.value()[0];
 
-        String id = parentPermission.getId() + permissionValue;
+        PermissionEntity childPermission = PermissionEntity.builder()
+                .microService(contextPath)
+                .parentId(parentPermission.getId())
+                .id(parentPermission.getId() + permissionValue)
+                .name(method.getName())
+                .code(permissionValue)
+                .path(path)
+                .description(description)
+                .type(httpMethod)
+                .build();
 
-        PermissionEntity permission =
-                permissionApi.findById(id, false).getData();
-
-        if (ObjectUtils.isEmpty(permission)) {
-
-            permission = permissionApi.insert(
-                    PermissionCreateDto.builder()
-                            .microService(contextPath)
-                            .parentId(parentPermission.getId())
-                            .id(id)
-                            .name(method.getName())
-                            .code(permissionValue)
-                            .path(path)
-                            .description(description)
-                            .type(httpMethod)
-                            .build()
-            ).getData();
-
-            log.info("insert children permission:{}", permission.getName());
-
-        } else {
-
-            permission = permissionApi.apply(
-                    PermissionUpdateDto.builder()
-                            .microService(contextPath)
-                            .parentId(parentPermission.getId())
-                            .id(id)
-                            .name(method.getName())
-                            .code(permissionValue)
-                            .path(path)
-                            .description(description)
-                            .type(httpMethod)
-                            .build()
-            ).getData();
-
-            log.info("apply children permission:{}", permission.getName());
+        permissions.put(childPermission.getId(), childPermission);
+        // 角色
+        if (ObjectUtils.isEmpty(saCheckPermission.orRole())) {
+            return;
+        }
+        for (String roleId : saCheckPermission.orRole()) {
+            if (roles.containsKey(roleId)) {
+                roles.get(roleId).getPermissions().add(childPermission);
+            } else {
+                List<PermissionEntity> rolePermissions = new ArrayList<>();
+                rolePermissions.add(childPermission);
+                roles.put(roleId, RoleEntity.builder()
+                        .id(roleId)
+                        .permissions(rolePermissions)
+                        .build());
+            }
         }
     }
 }
