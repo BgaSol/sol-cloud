9e28ce121fe963162b84b6dc18e926a6a3a4fa1f
sol <bgasolyang@gmail.com>
2026-07-06 12:47:08 +0800

    feat(plugin-discovery): Add local cache and optimize service registration mechanisms

--- cloud/plugin/plugin-discovery/plugin-discovery-starter/src/main/java/com/bgasol/common/core/base/scheduled/RegistryServer.java
+++ cloud/plugin/plugin-discovery/plugin-discovery-starter/src/main/java/com/bgasol/common/core/base/scheduled/RegistryServer.java
@@ -1,31 +1,58 @@
 package com.bgasol.common.core.base.scheduled;
 
 import com.bgasol.common.core.base.model.NodeConfig;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.redisson.api.RSetCache;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RTopic;
 import org.redisson.api.RedissonClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.bgasol.common.constant.value.SystemConfigValues.SERVICE_REGISTRY_KEY;
-
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RegistryServer {
-    private final ObjectMapper objectMapper;
+
     private final RedissonClient redissonClient;
     private final NodeConfig nodeConfig;
-    private final static int REGISTRY_TIMEOUT_SECONDS = 5;
 
-    @Scheduled(fixedRate = REGISTRY_TIMEOUT_SECONDS * 1000)
-    public void registryCurrentServer() throws JsonProcessingException {
-        RSetCache<String> registrySet = redissonClient.getSetCache(SERVICE_REGISTRY_KEY);
-        registrySet.add(objectMapper.writeValueAsString(nodeConfig),
-                REGISTRY_TIMEOUT_SECONDS + 1,
-                TimeUnit.SECONDS);
+    public final static String SERVICE_REGISTRY_KEY = "SERVICE_REGISTRY";
+    private final static int REGISTRY_BROADCAST_INTERVAL_SECONDS = 5;
+
+    @Getter
+    private final Cache<String, NodeConfig> nodeConfigCacheMap = Caffeine
+            .newBuilder()
+            .expireAfterWrite(REGISTRY_BROADCAST_INTERVAL_SECONDS * 2, TimeUnit.SECONDS)
+            .build();
+
+    private boolean online = false;
+
+    @PostConstruct
+    public void init() {
+        RTopic registryTopic = redissonClient.getTopic(SERVICE_REGISTRY_KEY);
+        registryTopic.addListener(NodeConfig.class, (channel, message) -> {
+            nodeConfigCacheMap.put(message.getAppName() + ":" + message.getName(), message);
+        });
+        online = true;
+        registryCurrentServer();
     }
+
+    @Scheduled(fixedRate = REGISTRY_BROADCAST_INTERVAL_SECONDS * 1000)
+    public void registryCurrentServer() {
+        if (!online) {
+            return;
+        }
+        redissonClient.getTopic(SERVICE_REGISTRY_KEY).publish(nodeConfig);
+    }
+
+    public boolean isOnLine(String appName, String name) {
+        return nodeConfigCacheMap.asMap().containsKey(appName + ":" + name);
+    }
+
 }
